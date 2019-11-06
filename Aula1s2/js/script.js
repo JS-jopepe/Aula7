16ec913ba88c0b2bb199c60975a17168dbb91f41
devdantas <jovenel.s@outlook.com>
2019-11-05 22:13:06 -0300

    usar script.js:)

---
+++ Aula1s2/js/script.js
@@ -0,0 +1,13 @@
+// defina login e senha
+
+var logPessoa = "Jopepe";
+var logSenha = 32075730;
+
+var j = prompt("Digite seu nome");
+var s = prompt("Digite sua senha");
+
+if(j == logPessoa && s == logSenha){
+    alert("Parabéns");
+} else {
+    alert(":( No succes")
+}
